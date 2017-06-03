441a5a24859075e7f83dd5cb669029e3eab654d8
Vladimir <vlado@itgbg.com>
2017-06-03 20:21:52 +0300

    Fixing solution for problem 10110. The only thing needed to check if a number has a even number of divisors is to check if it is perfect square.

--- 10110/main.cpp
+++ 10110/main.cpp
@@ -1,23 +1,12 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 typedef unsigned long long ull;
 
-ull getDivisorCount(ull x) {
-	int cnt = 1;
-	if (x != 1) {
-		++cnt;
-	}
-
-	for (ull i = 2; i * i <= x; i++) {
-		cnt += 2;
-		if (i * i == x) {
-			--cnt;
-		}
-	}
-
-	return cnt;
+bool hasOddDivisorCount(ull x) {
+	return ((ull)sqrt(x)) * ((ull)sqrt(x)) == x;
 }
 
 int main() {
@@ -26,7 +15,7 @@ int main() {
 		if (n == 0) {
 			break;
 		}
-		if (getDivisorCount(n) % 2 == 0) {
+		if (!hasOddDivisorCount(n)) {
 			cout << "no" << endl;
 		} else {
 			cout << "yes" << endl;
